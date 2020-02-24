3d5fd59eb211192776b71895848a453ed2ccc413
wenyan-zhang <54949925+wenyan-zhang@users.noreply.github.com>
2020-02-24 01:17:56 -0500

    Add files via upload

--- HW2.py
+++ HW2.py
@@ -1,4 +1,25 @@
-i = open("Step1Data.txt", "r")
+try:
+    i = open("Step1Data.txt", "r")
+    tempdic = {}
+    booklist = []
+    line = i.readlines()
+    
+    for x in range(1,len(line),6):
+        for y in range(5):
+            kv = line[x].split(': ')
+            tempdic[kv[0]] = kv[1]
+            x+=1
+        booklist.append(tempdic)
+        print(booklist)
+    print(booklist)                         
+except:
+    print("error")
 
-for line in i.readlines()
-        print(line)
+# print(booklist)
+# def displayBook(name):
+#     for book in booklist:
+#         print(book['Key'])
+#         if book["Key"]==name: 
+#             print('a')
+# 
+# displayBook("Rowling1997") 
